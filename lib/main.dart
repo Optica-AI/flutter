a691fc515a08e27b9ef6cb07152538388d98e660
Judy-Yeboah <jyeboah513@gmail.com>
2024-07-04 14:09:37 +0000

    theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const String appTitle = 'Home Page';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+void main(){
+  runApp(MyApp());
 }
 
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  location,
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: "Optica",
+      theme: ThemeData(
+        primarySwatch: Colors.white70,
+        primaryColor: Colors.deepPurple.shade400,
+        accentColor: Colors.deepPurple.shade100,
+        fontFamily: GoogleFonts.nunito(),
+        appBarTheme: Colors.white70,
+
       ),
     );
   }
