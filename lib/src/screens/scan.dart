086018b5a44527d8173f010994519616dbd36ece
Nana Kwasi <61452366+nanakwasi-0@users.noreply.github.com>
2024-07-29 23:47:46 +0100

    tflite_flutter

--- lib/src/screens/scan.dart
+++ lib/src/screens/scan.dart
@@ -4,6 +4,10 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:mime/mime.dart';
+
 
 class ScanScreen extends StatefulWidget {
   @override
@@ -68,6 +72,55 @@ class _ScanScreenState extends State<ScanScreen> {
     });
   }
 
+  Future<void> _savePhoto() async {
+    if(imagePath !=null){
+        try{
+          final uri = Uri.parse('http://10.0.2.2:3000/scans/upload');
+          final request = http.MultipartRequest ('POST', uri);
+
+          final imageFile = File(imagePath!);
+          final mimeType = lookupMimeType(imagePath!) ?? 'image/jpeg';
+          final imageStream = http.ByteStream(imageFile.openRead());
+          final imageLength = await imageFile.length();
+
+
+          final multipartFile = http.MultipartFile(
+            'image',
+            imageStream,
+            imageLength,
+            filename: path.basename(imagePath!),
+            contentType: MediaType.parse(mimeType),
+          );
+
+          request.files.add(multipartFile);
+          final response = await request.send();
+          print(response);
+
+          print('Uploading file: ${imagePath!}');
+          print('Mime type: $mimeType');
+          print('Response status: ${response.statusCode}');
+          final responseBody = await response.stream.bytesToString();
+          print('Response body: $responseBody');
+
+          if(response.statusCode == 200){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Photo uploaded successfully!')),
+            );
+          }
+          else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to upload photo.'))
+                );
+          }
+        }
+        catch (e){
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Error uploading photo: $e')),
+            );
+        }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,9 +163,27 @@ class _ScanScreenState extends State<ScanScreen> {
                             ),
                           ),
                         ),
-                        ElevatedButton(
-                          onPressed: _takePicture,
-                          child: Text('Take Photo'),
+                        Padding(
+                            padding: EdgeInsets.only(left: 0.0, top: 20.0, right: 0.0, bottom: 0.0),
+                            child: ElevatedButton(
+                                onPressed: _takePicture,
+                                style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(100, 100),
+                                    padding: EdgeInsets.zero,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(50),
+                                        side: BorderSide(
+                                            color: Colors.deepPurple,
+                                            width: 4,
+                                        ),
+                                    ),
+                                ),
+                                child: Icon(
+                                    Icons.camera_alt,
+                                    size: 30,
+                                    color: Colors.deepPurple
+                                ),
+                            ),
                         ),
                       ],
                     );
@@ -131,12 +202,12 @@ class _ScanScreenState extends State<ScanScreen> {
                         width: MediaQuery.of(context).size.width,
                         height: MediaQuery.of(context).size.height * 0.7,
                         child: FittedBox(
-                            fit: BoxFit.cover,
+                            fit: BoxFit.contain,
                             child: Transform.rotate(
                                 angle: 3 * 3.1415926535897932 / 2,
                                 child: Container(
-                                    width: controller.value.previewSize!.height,
-                                    height: controller.value.previewSize!.height,
+//                                     width: controller.value.previewSize!.height,
+                                    height: 600,
                                     child: Image.file(
                                         File(imagePath!),
                                     ),
@@ -153,9 +224,7 @@ class _ScanScreenState extends State<ScanScreen> {
                         child: Text('Retake Photo'),
                       ),
                       ElevatedButton(
-                        onPressed: () {
-                          // Handle saving the image
-                        },
+                        onPressed: _savePhoto,
                         child: Text('Save Photo'),
                       ),
                     ],
